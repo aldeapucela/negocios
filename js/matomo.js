3c2132afa055cfa64465e3f5fbdaf711eabda1ed
nukeador <nukeador@protonmail.com>
2025-07-13 23:03:24 +0200

    matomo id fix

--- js/matomo.js
+++ js/matomo.js
@@ -5,7 +5,7 @@ _paq.push(['enableLinkTracking']);
 (function() {
     var u="https://stats.aldeapucela.org/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '15']);
+    _paq.push(['setSiteId', '16']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
